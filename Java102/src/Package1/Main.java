9fe4dc675b3f7d735b2b8f9537159bec1f161635
aleyna-soyer <aleynasoyer9@gmail.com>
2023-02-27 23:59:53 +0300

    Packages

---
+++ Java102/src/Package1/Main.java
@@ -0,0 +1,11 @@
+package Package1;
+
+import Package2.Student;
+
+public class Main {
+	public static void main(String[] args) {
+		Student student= new Student("Aleyna Soyer");
+		student.study();
+	}
+
+}
